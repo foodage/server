5ee9b42d27b04a0e6731fc7de77c8297bad71be9
BOX12 <box0_@naver.com>
2023-08-31 10:46:44 +0900

    🍎 chore: 패키지 경로의 팀명 변경 (foodage -> fourdays)

--- src/test/java/com/fourdays/foodage/FoodageApplicationTests.java
+++ src/test/java/com/fourdays/foodage/FoodageApplicationTests.java
@@ -1,4 +1,4 @@
-package com.foodage.foodage;
+package com.fourdays.foodage;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FoodageApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
