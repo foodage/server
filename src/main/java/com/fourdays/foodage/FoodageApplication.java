44e66d61e7cd4f68a699d13a48d298f337f15095
BOX12 <box0_@naver.com>
2023-10-19 15:25:34 +0900

    🚀 feat: swagger 추가 (spring doc)
    ticket: #3

--- src/main/java/com/fourdays/foodage/FoodageApplication.java
+++ src/main/java/com/fourdays/foodage/FoodageApplication.java
@@ -2,9 +2,10 @@ package com.fourdays.foodage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EnableWebMvc
 public class FoodageApplication {
 
---
+++ src/main/java/com/fourdays/foodage/common/config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package com.fourdays.foodage.common.config;// Java
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	public OpenAPI openAPI() {
+		return new OpenAPI()
+			.components(new Components())
+			.info(apiInfo());
+	}
+
+	private Info apiInfo() {
+		String description =
+			"## Foodage API Docs\n" +
+				"``Hello!``\n";
+
+		return new Info()
+			.title("Foodage")
+			.description(description)
+			.version("1.0.0");
+	}
+}
