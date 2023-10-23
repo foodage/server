375f09c166cb63e4d932fa96504a64c9a3dac96f
BOX12 <box0_@naver.com>
2023-10-23 18:41:59 +0900

    🚀 feat: date 정보 입력을 위한 설정 추가 (@CreatedDate)

--- src/main/java/com/fourdays/foodage/FoodageApplication.java
+++ src/main/java/com/fourdays/foodage/FoodageApplication.java
@@ -3,9 +3,11 @@ package com.fourdays.foodage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@EnableJpaAuditing
 @EnableWebMvc
 public class FoodageApplication {
 
