6ab304745a16291692b6c3da73c87f30aaa5fb0e
BOX12 <box0_@naver.com>
2023-10-26 13:30:45 +0900

    🚀 feat: user 닉네임 생성 조건 추가
    ticket: #4

--- src/main/java/com/fourdays/foodage/user/controller/command/UserCreateRequest.java
+++ src/main/java/com/fourdays/foodage/user/controller/command/UserCreateRequest.java
@@ -1,6 +1,7 @@
 package com.fourdays.foodage.user.controller.command;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -12,6 +13,7 @@ public class UserCreateRequest {
 
 	@NotBlank(message = "닉네임을 입력해주세요.")
 	@Size(min = 2, max = 8, message = "닉네임은 2~8글자 사이로 입력해주세요. (예: 푸디지)")
+	@Pattern(regexp = "^[a-zA-Z0-9가-힣]+$", message = "닉네임에 사용할 수 없는 문자가 포함되어 있습니다. 다시 입력해주세요.")
 	private String nickname;
 
 	private String profileUrl;
