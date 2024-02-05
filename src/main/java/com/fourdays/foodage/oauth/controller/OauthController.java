a7ced1dbdbb284cdc63715dd31436cbe9ef28d1e
B0X12 <box0_@naver.com>
2024-02-05 11:38:36 +0900

    🧠 refactor: api 인터페이스 삭제

--- src/main/java/com/fourdays/foodage/oauth/controller/OauthController.java
+++ src/main/java/com/fourdays/foodage/oauth/controller/OauthController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +14,13 @@ import com.fourdays.foodage.oauth.dto.OauthLoginResponseDto;
 import com.fourdays.foodage.oauth.service.OauthService;
 import com.fourdays.foodage.oauth.util.OauthServerType;
 
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @Slf4j
-public class OauthController implements OauthApi {
+public class OauthController {
 
 	private final OauthService oauthService;
 
@@ -26,17 +28,18 @@ public class OauthController implements OauthApi {
 		this.oauthService = oauthService;
 	}
 
-	@Override // @GetMapping("/oauth/{oauthServerType}")
+	@Operation(summary = "Oauth 서비스 연동 URI 조회")
+	@GetMapping("/oauth/{oauthServerType}")
 	public ResponseEntity<String> getRequestUrl(@PathVariable OauthServerType oauthServerType
-		// HttpServletResponse response
 	) {
 
 		String redirectUrl = oauthService.getRequestUri(oauthServerType);
-		// response.sendRedirect(redirectUrl);
 		return ResponseEntity.ok().body(redirectUrl);
 	}
 
-	@Override // @GetMapping("/oauth/{oauthServerName}/login")
+	// oauth -> (backend) redirect url로 전달하는 auth code를 receive & 사용자 정보 받아 login
+	@Operation(hidden = true)
+	@GetMapping("/oauth/{oauthServerName}/login")
 	public ResponseEntity<OauthLoginResponseDto> login(@PathVariable String oauthServerName,
 		@RequestParam String code, HttpServletResponse response) throws IOException {
 
@@ -53,7 +56,7 @@ public class OauthController implements OauthApi {
 		}
 
 		// 회원가입 페이지로 이동시킴
-		// String redirectUrl = "http://localhost:3000/tutorial";
+		String redirectUrl = "http://localhost:3000/tutorial";
 		// response.sendRedirect(redirectUrl);
 		return ResponseEntity.ok().headers(httpHeaders).body(result);
 	}
