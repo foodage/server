ff874b6a53464a7c8bb3a92d8b58f693831e6ced
B0X12 <box0_@naver.com>
2024-02-19 14:37:09 +0900

    🚀 feat: redis 설정 추가
    ticket: #22

---
+++ src/main/java/com/fourdays/foodage/common/config/RedisConfig.java
@@ -0,0 +1,23 @@
+package com.fourdays.foodage.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+	@Bean
+	public RedisTemplate<Long, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+
+		RedisTemplate<Long, Object> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(connectionFactory);
+		redisTemplate.setKeySerializer(new StringRedisSerializer());
+		redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+		return redisTemplate;
+	}
+}
