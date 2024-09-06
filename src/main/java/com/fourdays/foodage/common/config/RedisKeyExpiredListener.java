64445de5500086d2895a6d10f361c111240e2b9a
B0X12 <box0_@naver.com>
2024-09-06 16:55:39 +0900

    🚀 feat: 회원 탈퇴 완료 함수 내 예외처리 추가
    * 회원 탈퇴 완료 처리 중 문제 발생한 경우, attention log 데이터 보고 수동 업데이트 필요
    
    issue: #54

--- src/main/java/com/fourdays/foodage/common/config/RedisKeyExpiredListener.java
+++ src/main/java/com/fourdays/foodage/common/config/RedisKeyExpiredListener.java
@@ -1,5 +1,7 @@
 package com.fourdays.foodage.common.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
@@ -8,6 +10,7 @@ import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.stereotype.Component;
 
 import com.fourdays.foodage.common.enums.RedisKeyType;
+import com.fourdays.foodage.common.exception.FoodageException;
 import com.fourdays.foodage.member.service.MemberCommandService;
 import com.fourdays.foodage.member.vo.MemberId;
 import com.fourdays.foodage.oauth.util.OauthServerType;
@@ -19,6 +22,8 @@ import lombok.extern.slf4j.Slf4j;
 public class RedisKeyExpiredListener extends KeyExpirationEventMessageListener {
 
 	private final MemberCommandService memberCommandService;
+
+	private static final Logger attentionLog = LoggerFactory.getLogger("attentionLog");
 
 	/**
 	 * Creates new {@link MessageListener} for {@code __keyevent@*__:expired} messages.
@@ -51,7 +56,25 @@ public class RedisKeyExpiredListener extends KeyExpirationEventMessageListener {
 			OauthServerType oauthServerType = OauthServerType.fromName(parts[1]);
 			String accountEmail = parts[3];
 
-			memberCommandService.completeLeave(new MemberId(oauthServerType, accountEmail));
+			try {
+				memberCommandService.completeLeave(new MemberId(oauthServerType, accountEmail));
+			} catch (FoodageException e) {
+				log.error("@ ");
+				attentionLog.info("");
+				attentionLog.info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+				attentionLog.error("[attention!] completeLeave()에서 에러 발생");
+				attentionLog.error("@ 회원 탈퇴 처리 중 문제가 발생하였습니다. 매뉴얼에 따라 데이터를 수동으로 업데이트하세요.");
+				attentionLog.error("* errorCode: {}", e.getErrCode().getCode());
+				attentionLog.error("* errorMessage: {}", e.getErrCode().getMessage());
+				attentionLog.info("");
+				attentionLog.info("--- [info] ---");
+				attentionLog.info("* redisKey        : {}", redisKey); // already deleted
+				attentionLog.info("* oauthServerType : {}", oauthServerType);
+				attentionLog.info("* accountEmail    : {}", accountEmail);
+				attentionLog.info("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+				attentionLog.info("");
+			}
+
 		}
 	}
 }
